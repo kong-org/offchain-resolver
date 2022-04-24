4811a8f4eb9540716ffaed5494fae594b942941c
Nico Ramirez <ramicaza@gmail.com>
2022-04-23 23:53:03 -0400

    L2KongResolver and slashing tests done

--- packages/contracts/contracts/L2KongResolver.sol
+++ packages/contracts/contracts/L2KongResolver.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "./RegistryForwarder.sol";
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract L2KongResolver is RegistryForwarder {
     address _bridger;
@@ -27,7 +27,8 @@ contract L2KongResolver is RegistryForwarder {
     // TODO: code permissions in here
     function setTsm(bytes32 chipId, address newTsm) public {
         _tsms[chipId] = newTsm;
-        _recordHistoricalEntry(chipId, this.tsm.selector, keccak256(abi.encodePacked(newTsm)));
+        bytes memory getterCalldata = abi.encodeWithSelector(this.tsm.selector, chipId);
+        _recordHistoricalEntry(getterCalldata, keccak256(abi.encodePacked(newTsm)));
     }
 
     // END TSM STUFF
@@ -42,38 +43,34 @@ contract L2KongResolver is RegistryForwarder {
     // TODO: code permissions in here
     function setApp(bytes32 chipId, string calldata newApp) public {
         _apps[chipId] = newApp;
-        _recordHistoricalEntry(chipId, this.app.selector, keccak256(abi.encodePacked(newApp)));
+        bytes memory getterCalldata = abi.encodeWithSelector(this.app.selector, chipId);
+        _recordHistoricalEntry(getterCalldata, keccak256(abi.encodePacked(newApp)));
     }
 
     // END APP STUFF
 
-    event RecordUpdate(uint32 timestamp, bytes32 chipId, bytes32 entryHash);
+    event RecordUpdate(uint32 timestamp, bytes getterCalldata, bytes32 recordHash);
 
-    // bytes14 is used here to squeeze HistoricalEntry into one 32 byte word
+    // bytes28 is used here to squeeze HistoricalEntry into one 32 byte word
     struct HistoricalEntry {
         uint32 timestamp;
-        bytes14 chipId; // leading 14 bytes of chipid
-        bytes14 entryHash; // leading 14 bytes of entryHash
+        bytes28 recordHash; // leading 14 bytes of recordHash
     }
-    // map from getter selector -> historical entries array
+    // map from hash(calldata) -> historical entries array
     // E.g. tsm(bytes32 chipId) might have selector 0x69694242 which would be the key in this map
-    mapping(bytes4 => HistoricalEntry[]) _historicalEntries;
+    mapping(bytes32 => HistoricalEntry[]) _historicalEntries;
 
-    function _recordHistoricalEntry(
-        bytes32 chipId,
-        bytes4 getterSelector,
-        bytes32 entryHash
-    ) internal {
-        _historicalEntries[getterSelector].push(
-            HistoricalEntry(uint32(block.timestamp), bytes14(chipId), bytes14(entryHash))
-        );
-        emit RecordUpdate(uint32(block.timestamp), chipId, entryHash);
+    // TODO: determine whether should emit entry itself or the hash
+    function _recordHistoricalEntry(bytes memory getterCalldata, bytes32 recordHash) internal {
+        bytes32 calldataHash = keccak256(getterCalldata);
+        _historicalEntries[calldataHash].push(HistoricalEntry(uint32(block.timestamp), bytes28(recordHash)));
+        emit RecordUpdate(uint32(block.timestamp), getterCalldata, recordHash);
     }
 
     /* 
         To spank the bridger, one must:
         1. Have a resolution signed by the bridger. That is a timestamp guarantee G, chipId C, and entry data D
-        2. Find a Historical entry with timestamp T, chipId C' and entryHash H that satisfies the following conditions:
+        2. Find a Historical entry with timestamp T, chipId C' and recordHash H that satisfies the following conditions:
             a. C' == C
             b. T <= G
             c. It is the entry with the highest index in _historicalEntries that satisfies conditions (a) and (b)
@@ -82,21 +79,19 @@ contract L2KongResolver is RegistryForwarder {
         If there is no historical entry with C' == C but there's a valid signature from the bridger, spanking may occur
     */
     function slashBridger(
-        bytes4 getterSelector,
         uint32 guaranteeTimestamp,
-        bytes32 chipId,
-        bytes32 entryHash, // the data the bridger alledgedly resolved
+        bytes calldata getterCalldata,
+        bytes calldata packedReturnVal, // the data the bridger alledgedly resolved packed using abi.encodePacked
         // parameters for signature from bridger over timestamp, chipId, <entry fields>
         uint8 v,
         bytes32 r,
         bytes32 s
     ) public {
         // console.log("Attempting spank");
-        // A hash of data that includes a hash of data that includes a hash, damn
         bytes32 signatureHash = keccak256(
             abi.encodePacked(
                 "\x19Ethereum Signed Message:\n32",
-                keccak256(abi.encodePacked(guaranteeTimestamp, chipId, entryHash))
+                keccak256(abi.encodePacked(guaranteeTimestamp, getterCalldata, packedReturnVal))
             )
         );
         address signer = ecrecover(signatureHash, v, r, s);
@@ -106,41 +101,55 @@ contract L2KongResolver is RegistryForwarder {
             // if the bridger didn't sign the message, we can't spank him
             return;
         }
-        if (
-            getterSelector == RegistryForwarder.manufacturer.selector ||
-            getterSelector == RegistryForwarder.ellipticCurve.selector ||
-            getterSelector == RegistryForwarder.stakingExpiration.selector ||
-            getterSelector == RegistryForwarder.resolver.selector
-        ) {
-            // TODO: special case because these are all immutable so no need to check records
+
+        bytes32 recordHash = keccak256(packedReturnVal);
+        bytes32 calldataHash = keccak256(getterCalldata);
+        uint8 forwardedFunctionResult = _checkForwardedFunctions(getterCalldata, recordHash);
+        if (forwardedFunctionResult == 1) {
+            return;
+        } else if (forwardedFunctionResult == 0) {
+            _performSlash();
         }
 
         // annoying ugly for loop iteration logic because of solidity's overflow protection
-        for (uint256 b = 0; b < _historicalEntries[getterSelector].length; ++b) {
-            uint256 i = _historicalEntries[getterSelector].length - b - 1;
+        for (uint256 b = 0; b < _historicalEntries[calldataHash].length; ++b) {
+            uint256 i = _historicalEntries[calldataHash].length - b - 1;
             // console.log("i", i);
-            if (_historicalEntries[getterSelector][i].timestamp > guaranteeTimestamp) {
+            if (_historicalEntries[calldataHash][i].timestamp > guaranteeTimestamp) {
                 // loop until we get to the entries BEFORE the guarantee timestamp
                 // console.log(_historicalEntries[i].timestamp);
                 continue;
             }
-            // go back from the guarantee time until we get to the last update to the chip in question
-            if (_historicalEntries[getterSelector][i].chipId == bytes14(chipId)) {
-                // if the data is wrong in the slightest way we gotta spank
-                if (_historicalEntries[getterSelector][i].entryHash != bytes14(entryHash)) {
-                    _performSlash();
-                }
-                return;
+            // if the data is wrong in the slightest way we gotta spank
+            if (_historicalEntries[calldataHash][i].recordHash != bytes28(recordHash)) {
+                _performSlash();
             }
+            return;
         }
         // if we got to the end that means the bridger signed a message for an invalid chipId, a big nono
         _performSlash();
         return;
     }
 
+    function _checkForwardedFunctions(bytes calldata getterCalldata, bytes32 recordHash) internal view returns (uint8) {
+        bytes4 getterSelector = bytes4(getterCalldata[0:4]);
+        bytes32 chipId = bytes32(getterCalldata[4:36]);
+
+        if (getterSelector == RegistryForwarder.manufacturer.selector) {
+            return keccak256(abi.encodePacked(manufacturer(chipId))) == recordHash ? 1 : 0;
+        } else if (getterSelector == RegistryForwarder.ellipticCurve.selector) {
+            return keccak256(abi.encodePacked(ellipticCurve(chipId))) == recordHash ? 1 : 0;
+        } else if (getterSelector == RegistryForwarder.stakingExpiration.selector) {
+            return keccak256(abi.encodePacked(stakingExpiration(chipId))) == recordHash ? 1 : 0;
+        } else if (getterSelector == RegistryForwarder.resolver.selector) {
+            return keccak256(abi.encodePacked(resolver(chipId))) == recordHash ? 1 : 0;
+        }
+        return 2;
+    }
+
     function _performSlash() internal {
         // TODO: spank the bridger
-        // console.log("SPANK");
+        console.log("SPANK");
         // Send it all out to mr spanker. well played mr spanker.
         payable(msg.sender).call{value: address(this).balance}("");
     }
